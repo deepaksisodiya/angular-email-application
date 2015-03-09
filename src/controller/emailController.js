4ad1ff8512e11c3546705b2b602abf20e5803b70
Deepak <deepak.sisodiya@gmail.com>
2015-03-09 14:03:44 +0530

    done

--- src/controller/emailController.js
+++ src/controller/emailController.js
@@ -1,3 +1,15 @@
 /**
  * Created by Deepak Sisodiya on 09/03/15.
  */
+
+app.controller('emailController', ['$scope', 'emailFactory', function ($scope, $emailFactory) {
+
+  $emailFactory.getMessages()
+    .success(function(data, status) {
+      console.log("Successful : ", status);
+      $scope.data = {
+        email : data
+      };
+    });
+
+}]);
--- src/controller/inboxController.js
+++ src/controller/inboxController.js
@@ -1,3 +1,25 @@
 /**
  * Created by Deepak Sisodiya on 08/03/15.
  */
+
+app.controller('inboxController', ['$scope', 'inboxFactory', function ($scope, inboxFactory) {
+  $scope.meta = {
+    title : "My Inbox"
+  };
+  inboxFactory.getMessages()
+    .success(function(data, status) {
+      console.log("Successful : ", status);
+      $scope.data = {
+        emails : data
+      };
+    });
+
+  $scope.deleteMessage = function(id, index) {
+    inboxFactory.deleteMessage(id, index, $scope)
+  };
+
+  $scope.goToMessage = function(id) {
+    inboxFactory.goToMessage(id);
+  };
+
+}]);
