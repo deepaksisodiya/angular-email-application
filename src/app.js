a9fb8e459f006aff0e698f70a62873ac3be539c3
Deepak <deepak.sisodiya@gmail.com>
2015-03-09 13:59:54 +0530

    done

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 /**
  * Created by Deepak Sisodiya on 08/03/15.
  */
+
+var app = angular.module("myApp", ['ui.router', 'ngSanitize']);
+app.config(function($stateProvider, $urlRouterProvider) {
+  $urlRouterProvider.otherwise('/inbox');
+  $stateProvider
+    .state('inbox', {
+      url : '/inbox',
+      templateUrl : './src/view/inbox.html',
+      controller : 'inboxController'
+    })
+    .state('inbox/email/:id', {
+      url : '/inbox/email/:id',
+      templateUrl : './src/view/email.html',
+      controller : 'emailController'
+    })
+});
