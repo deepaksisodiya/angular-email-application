8ef8bb0fa011814ba3eba01b008724afd678c1c8
Deepak <deepak.sisodiya@gmail.com>
2015-03-09 14:03:16 +0530

    done

--- src/factory/emailFactory.js
+++ src/factory/emailFactory.js
@@ -1,3 +1,15 @@
 /**
  * Created by Deepak Sisodiya on 09/03/15.
  */
+
+app.factory('emailFactory', ['$http', '$stateParams', function($http, $stateParams) {
+  "use strict";
+  var exports = {};
+  exports.getMessages = function() {
+  console.log($stateParams.id);
+  if($stateParams.id) {
+    return $http.get('/json/emails/' + $stateParams.id + '.json');
+  }
+  };
+  return exports;
+}]);
--- src/factory/inboxFactory.js
+++ src/factory/inboxFactory.js
@@ -1,3 +1,25 @@
 /**
  * Created by Deepak Sisodiya on 08/03/15.
  */
+
+app.factory('inboxFactory', ['$http', '$location', function($http, $location) {
+  "use strict";
+  var exports = {};
+  exports.getMessages = function() {
+    return $http.get('/json/emails.json');
+  };
+
+  exports.deleteMessage = function(id, index, $scope) {
+    $scope.data.emails.splice(index, 1);
+  };
+
+  exports.goToMessage = function(id) {
+    if(angular.isNumber(id)) {
+      console.log('inbox/email/' + id);
+      $location.path('/inbox/email/' + id);
+
+    }
+  };
+
+  return exports;
+}]);
